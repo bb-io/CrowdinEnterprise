b925cb3939ad5f9884d9b5f0b21eb462967a21dc
bZverok <bohdan.zverok@blackbird.io>
2023-09-27 16:48:29 +0300

    fixed namings + added connection validator

---
+++ Apps.CrowdinEnterprise/Connections/ConnectionValidator.cs
@@ -0,0 +1,32 @@
+﻿using Apps.CrowdinEnterprise.Api;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.CrowdinEnterprise.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        var client = new CrowdinEnterpriseClient(authProviders.ToArray());
+
+        try
+        {
+            await client.Languages.ListSupportedLanguages();
+
+            return new()
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
