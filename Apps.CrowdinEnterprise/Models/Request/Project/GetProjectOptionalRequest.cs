685132a4eb972e4ee5dcf46018a35357ee88c084
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-10 12:44:02 +0300

    Preparations for checkpoints

---
+++ Apps.CrowdinEnterprise/Models/Request/Project/GetProjectOptionalRequest.cs
@@ -0,0 +1,12 @@
+﻿using Apps.CrowdinEnterprise.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.CrowdinEnterprise.Models.Request.Project;
+
+public class GetProjectOptionalRequest
+{
+    [Display("Project")]
+    [DataSource(typeof(ProjectDataHandler))]
+    public string? ProjectId { get; set; }
+}
---
+++ Apps.CrowdinEnterprise/Models/Request/SourceString/GetSourceStringOptionalRequest.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.CrowdinEnterprise.Models.Request.SourceString;
+
+public class GetSourceStringOptionalRequest
+{
+    [Display("String ID")] 
+    public string? StringId { get; set; }
+}
